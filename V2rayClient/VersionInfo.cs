633fc72576757601ff0122fd8b47a7313122e37a
zhangjh <njhxzhangjihong@126.com>
2025-10-31 12:38:53 +0800

    update version

--- V2rayClient/VersionInfo.cs
+++ V2rayClient/VersionInfo.cs
@@ -10,7 +10,7 @@ public static class VersionInfo
     /// V2rayZ 应用版本号 - 唯一的版本号，用于更新检查
     /// 修改版本时只需要更新这个值
     /// </summary>
-    public const string Version = "1.4.1";
+    public const string Version = "1.5.0";
 
     /// <summary>
     /// 应用程序名称
